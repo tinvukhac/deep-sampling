0f99ac3ecdc787cc03a219dc863e8f0a014ef88d
Tin Vu <tvu032@ucr.edu>
2020-05-08 23:55:29 -0700

    Load histograms

--- datasets.py
+++ datasets.py
@@ -1,8 +1,21 @@
+import numpy as np
 import pandas as pd
 
 
 def load_query_attributes(filename):
     cols = ['dataset_name', 'sampling_budget', 'query_ratio', 'mean_accuracy']
     df = pd.read_csv(filename, delimiter=',', header=None, names=cols)
-    df = df.drop(['dataset_name'], axis=1)
+    # df = df.drop(['dataset_name'], axis=1)
     return df
+
+
+def load_histograms(df, num_rows, num_columns):
+    histograms = []
+
+    histogram_dir = 'data/histogram_values/{}x{}'.format(num_rows, num_columns)
+
+    for filename in df['dataset_name']:
+        hist = np.genfromtxt('{}/{}'.format(histogram_dir, filename), delimiter=',')
+        histograms.append(hist)
+
+    return histograms
--- training.py
+++ training.py
@@ -4,7 +4,7 @@ import datasets
 def main():
     print ("Train and test a prediction model for SE problem")
     df = datasets.load_query_attributes('data/query_accuracies.csv')
-    print (df)
+    histograms = datasets.load_histograms(df, 16, 16)
 
 
 if __name__ == "__main__":
